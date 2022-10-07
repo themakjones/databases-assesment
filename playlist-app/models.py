dc6992b4be00eb7977c339f94c257986a977466f
Mak Jones <jonesmakalyn@gmail.com>
2022-10-07 16:00:55 -0400

    Created models

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -1,26 +1,49 @@
 """Models for Playlist app."""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
+
 
 
 class Playlist(db.Model):
     """Playlist."""
 
     # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(100))
 
 
 class Song(db.Model):
     """Song."""
 
     # ADD THE NECESSARY CODE HERE
+    __tablename__ = "songs"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    artist = db.Column(db.String(50), nullable=False)
+
 
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
     # ADD THE NECESSARY CODE HERE
+    __tablename__ = 'playlists_songs'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'))
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
+
+    playlist = db.relationship('Playlist', back_populates='playlists_songs')
+    song = db.relationship('Song', back_populates='playlists_songs')
+
+
 
 
 # DO NOT MODIFY THIS FUNCTION
